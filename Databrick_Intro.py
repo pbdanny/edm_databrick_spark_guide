51862e527f35f66619ead5fc6bdd57acc5350f1d
pbdanny <pbdanny>
2022-12-04 03:59:36 +0000

    Add Databrick Intro

---
+++ Databrick_Intro.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Recommended resources
+# MAGIC 
+# MAGIC All resource : https://learn.microsoft.com/en-us/azure/databricks/  
+# MAGIC Azure Databrick concept : https://learn.microsoft.com/en-us/azure/databricks/getting-started/concepts  
+# MAGIC SparkDataFrame : https://learn.microsoft.com/en-us/azure/databricks/getting-started/dataframes-python  
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Databrick Environment (Live Demo)
+# MAGIC 
+# MAGIC 1. Workspace
+# MAGIC     - Create Notebook, Folder
+# MAGIC 2. Data
+# MAGIC     - Table : focus on `tdm_seg`
+# MAGIC     - DBFS : `FileStore`, `mnt` (advance `/user/hive/warehouse/tdm_seg.db`)
+# MAGIC 3. Compute
+# MAGIC     - Meta, 01, 02
+# MAGIC 4. Workflow
+
+# COMMAND ----------
+
+
